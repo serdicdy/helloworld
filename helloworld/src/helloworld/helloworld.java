9b615d0457bdc0ae0c0b35bcfb053a824c55b193
yj-602-12 <yj-602-12@yj-605-12>
2019-08-26 10:44:25 +0900

    hello

--- helloworld/src/helloworld/helloworld.java
+++ helloworld/src/helloworld/helloworld.java
@@ -5,6 +5,7 @@ public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!!");
 		System.out.println("HelloWorld!!");
+		System.out.println("헬로월드");
 	}
 
 }
